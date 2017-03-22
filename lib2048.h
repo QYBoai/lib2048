aab26c35d2a07ff03039cfe36d7a04abe910d2d2
郭俊余 <hagbgreen@yahoo.com>
2017-03-22 17:19:21 +0800

    Update lib2048.h

--- lib2048.h
+++ lib2048.h
@@ -77,15 +77,15 @@ struct L2_game{
 	L2_sv soc;	/*分数*/
 	L2_tab table;	/*棋盘*/
 	L2_sint num;	/*已占用格子数，值为17则游戏结束*/
-	L2_sint (*rand)(void *randi,L2_sint s,L2_sint e);	/*自定义随机函数，返回[s,e]之间的整数(s<=e)，game传入该游戏实例的指针*/
-	void (*rands)(void *randi);	/*自定义随机初始化函数，game传入该游戏实例的指针*/
+	L2_sint (*rand)(void *randi,L2_sint s,L2_sint e);	/*自定义随机函数，返回[s,e]之间的整数(s<=e)*/
+	void (*rands)(void *randi);	/*自定义随机初始化函数*/
 	void *randi;	/*供自定义随机函数使用的指针，与库无关*/
 };
 	/*struct L2_game	游戏实例*/
 typedef struct L2_game L2_game;
 	/*L2_game	同struct L2_game*/
 L2_EX L2_game *l2_init(L2_game *game,L2_sint (*rand)(void *randi,L2_sint s,L2_sint e),void (*rands)(void *randi),void *randi);
-	/*L2_game *l2_start(L2_game *game,L2_sint (*rand)(struct L2_game *game,L2_sint s,L2_sint e),void (*rands)(struct L2_game *game),void *randi)	初始化一个游戏实例(但不生成初始数格)；
+	/*L2_game *l2_init(L2_game *game,L2_sint (*rand)(void *randi,L2_sint s,L2_sint e),void (*rands)(void *randi),void *randi)	初始化一个游戏实例(但不生成初始数格)；
 			返回传入的指针*/
 L2_EX L2_game *l2_start(L2_game *game);
 	/*L2_game *l2_start(L2_game *game)	开始新游戏(生成初始数格)；
@@ -96,7 +96,7 @@ L2_EX L2_sint l2_my(L2_game *gmae);	/*向y正方向划动*/
 L2_EX L2_sint l2_fy(L2_game *game);	/*向y负方向划动*/
 	/*以上四个函数，若划动成功，返回已占用格子数，划动不成功返回0，游戏已结束则返回17*/
 L2_EX L2_sint l2_num(L2_game *game);
-	/*L2_sint l2_num(const L2_game *game)	 返回占用格子数；
+	/*L2_sint l2_num(L2_game *game)	 返回占用格子数；
 			游戏已结束返回17*/
 L2_EX L2_gb l2_new(L2_game *game);	
 	/*L2_gd l2_new(L2_game *game)	随机生成新数字。
@@ -113,7 +113,7 @@ L2_EX L2_tab *l2_get_table(const L2_game *game,L2_tab *table);
 	/*L2_tab *l2_get_table(const L2_game *game,L2_tab *table)	获取整个棋盘，table指针用于储存棋盘；
 			返回table指针*/
 L2_EX L2_game *l2_clone(const L2_game *game,L2_game *target,L2_sint (*rand)(void *randi,L2_sint s,L2_sint e),void (*rands)(void* randi),void *randi);
-	/*L2_tab *l2_clone(const L2_game *game,L2_game *target,L2_sint (*rand)(struct L2_game *game,L2_sint s,L2_sint e),void (*rands)(struct L2_game *game),void *randi)	克隆一个有相同数据(随机函数另外传入)的游戏实例；
+	/*L2_game *l2_clone(const L2_game *game,L2_game *target,L2_sint (*rand)(void *randi,L2_sint s,L2_sint e),void (*rands)(void* randi),void *randi)	克隆一个有相同数据(随机函数另外传入)的游戏实例；
 			返回target指针*/
 L2_EX L2_pv l2_set_pv(L2_game *game,const L2_gb gb);
 	/*L2_pv l2_set_pv(L2_game *game,const L2_gb gb)	设置特定坐标上格子的数值；
